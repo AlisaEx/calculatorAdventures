672cfc3d03002b2f8441f60c2db385869da18462
AlisaEx <eatliightning@gmail.com>
2013-07-25 14:22:35 -0400

    addition working

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ function segregate(array, index){
 		}
 	}
 	console.log(current);
-	removeQuotes(current);
+	loopThrough(current);
 }
 function removeSpace(array){
 	return array.filter(function(x){return x != " ";});
@@ -41,21 +41,60 @@ function combineNumbers(array){
 }
 
 function removeQuotes(string){
-	return JSON.parse(string);
+	if(string === "+"){
+		return string.charAt(0);
+	}
+	else{
+		return JSON.parse(string);
+	}
 }
 
 function interpret(array){
+	current = [];
 	for (i=0; i<array.length; i++){
-		console.log(array[i]);
+		if(array[i] instanceof Array && array[i].length > 0){
+			for (j=0; j<array[i].length; j++){
+				if(array[i][j]==="+"){
+					addToArray(current, (add(array[i][j-1], array[i][j+1])))
+
+				}
+			}
+		}
 		if(array[i]==="+"){
-			console.log("wut");
+			addToArray(current, (add(array[i-1], array[i+1])))
+			}
+		}
+	console.log(current);
+	current.reduce(add);
+	console.log(current.reduce(add));
+}
+
+function loopThrough(array){
+	for(i=0; i<array.length; i++){
+		if (array[i] instanceof Array){
+			for (j=0; j<array[i].length; j++){
+					if(array[i][j] != "+"){
+						array[i][j] = JSON.parse(array[i][j]);
+					}
+			}
 		}
 		else{
-			removeQuotes(array[i]);
+			if(array[i] != "+"){
+				array[i] = JSON.parse(array[i]);
+			}
 		}
 	}
-	console.log(current);
+	console.log(array);
+	interpret(array);
 }
+
 function add(x, y){
-	return x+y;
+	if(typeof x === 'number' && typeof y === 'number'){
+		return x+y;
+	}
+}
+function addToArray(array,item){
+	if (item != undefined){
+		array.push(item);
+	}
 }
