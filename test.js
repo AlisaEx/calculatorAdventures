9deee3a01085654a1d6bf52fedb3aed74275056a
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-08-07 12:15:07 -0400

    Make parenthesised input and order of ops work.

--- test.js
+++ test.js
@@ -1,12 +1,40 @@
 // Simple JavaScript Calculator
 // made by: Alisa Ex
 
+// RETURNS TRUE IF THE ELEMENT IS A NUMBER && NOT AN ARRAY
+function isNumber(n){
+	return !isNaN(parseInt(n)) && !(n instanceof Array);
+}
 
 
 // Takes a string value as input
 // && returns the result of that string
 
+// DICTIONARY FOR OPERATORS
+var operations = [{
+	name: "^",
+	fn: function (x,y){return Math.pow(x,y);}
+},{
+	name: "*",
+	fn: function (x,y){return x*y;}
+},{
+	name: "/",
+	fn: function(x,y){return x/y;}
+},{
+	name: "+",
+	fn: function(x,y){return x + y;}
+},{
+	name: "-",
+	fn: function(x,y){return x - y;}
+}]
 
+var findOperation = function(operatorName) {
+  for (var i = 0; i < operations.length; i++) {
+    if (operations[i].name === operatorName) {
+      return operations[i];
+    }
+  }
+};
 
 function parse(string){
 	result = [];
@@ -62,25 +90,67 @@ function segregate(array){
 // 	}
 // }
 
-function orderOperation(array){
-	for (var i=0 i< operations.length; i++){
-		array = applyOperator(array,operations[i]);
+function applyOperators(array){
+	for (var i=0; i < operations.length; i++){
+		array = applyOperator(array, operations[i]);
 	}
+  return array;
 }
+
+
+
 function applyOperator(array, operator){
 	if(array.length === 0){return array;}
-	if(array.length === 1){return array[0];}
-	if(array[1] === operator.name){
+	// if(array.length === 1){return array[0];}
+  if (array[0] instanceof Array && array[0].length > 1) {
+    var evaledArray = applyOperators(array[0]);
+    // console.log(evaledArray)
+    return applyOperator(evaledArray.concat(
+      array.slice(1)), operator);
+  } else if(array[1] === operator.name){
 		var answer = operator.fn(array[0], array[2]);
-		var rest = array.slice(3,-1);
+		var rest = array.slice(3);
 		return applyOperator([answer].concat(rest),operator);
 	}
-	else{
+	else {
 		var start = array.slice(0,2);
-		var rest = array.slice(3,-1);
-		return start.concat(applyOperator(rest,operator));
+		var rest = array.slice(2);
+		return start.concat(applyOperator(rest, operator));
 	}
 }
+
+var plus = findOperation("+");
+// console.log("test1:", applyOperator([1, "+", 3, "+", 5], plus));
+// console.log("test2:", applyOperator([1, "+", 3, "-", 5, "+", 7], plus));
+// console.log("test3:", applyOperator([1, "+", 3, "-", 5, "*", 7, "+", 9], plus));
+// console.log("test4:", applyOperators([[1, "+", 3], "-", 5, "*", 7, "+", 9]));
+// console.log("test5:", applyOperators([[1, "+", 3, "-", 5], "*", 7, "+", 9]));
+console.log("test6:", applyOperators([[1, "*", [3, "-", 5]], "*", 7, "+", 9]));
+
+
+
+var evalParentheses = function(array) {
+  if (array.length === 0) return [];
+  if (array.length === 1) return array[0];
+  if (array[0] instanceof Array) { // is array
+    applyOperators(array[0]);
+    return evalParentheses(evalParentheses(array[0]).concat(
+      array.slice(1)));
+  } else { // number or operator
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // RETURNS ARRAY AFTER NUMBER PARSING
@@ -96,5 +166,3 @@ function loopThrough(array){
 	}
 	return array;
 }
-
-
